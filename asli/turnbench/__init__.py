f294ce41c4c0529e3cd688ae3c58d9c4efb20a18
Riya Dadlani <riyadadlani@Riyas-MacBook-Air.local>
2026-08-21 16:20:40 +0530

    feat: add TurnBench automatic reference records

--- asli/turnbench/__init__.py
+++ asli/turnbench/__init__.py
@@ -1,6 +1,22 @@
 """Versioned records used by the offline TurnBench boundary."""
 
 from .candidates import PauseCandidate, extract_candidates
+from .auto_schema import (
+    AutoPrediction,
+    CANDIDATE_SCHEMA,
+    PREDICTION_SCHEMA,
+    REFERENCE_SCHEMA,
+    REFERENCE_SOURCE,
+    DiarBenchCandidate,
+    DiarBenchReference,
+    read_candidates,
+    read_predictions,
+    read_references,
+    write_candidates,
+    write_predictions,
+    write_references,
+)
+from .diarbench import convert_diarbench_sample
 from .report import score_inputs
 from .schema import (
     Annotation,
@@ -20,23 +36,37 @@ from .score import DecisionScore, aggregate, bootstrap_by_recording, nearest_ran
 
 __all__ = [
     "Annotation",
+    "AutoPrediction",
+    "CANDIDATE_SCHEMA",
     "DecisionLabel",
     "DecisionScore",
+    "DiarBenchCandidate",
+    "DiarBenchReference",
     "EVENT_SCHEMA",
     "LABEL_SCHEMA",
     "ProviderTrace",
     "PauseCandidate",
+    "PREDICTION_SCHEMA",
     "RECORDING_SCHEMA",
+    "REFERENCE_SCHEMA",
+    "REFERENCE_SOURCE",
     "Recording",
     "SchemaError",
     "SourceTurn",
     "TraceEvent",
     "aggregate",
     "bootstrap_by_recording",
+    "convert_diarbench_sample",
     "extract_candidates",
     "nearest_rank",
     "read_jsonl",
+    "read_candidates",
+    "read_predictions",
+    "read_references",
     "score_decision",
     "score_inputs",
     "write_jsonl",
+    "write_candidates",
+    "write_predictions",
+    "write_references",
 ]
